c28dca29e08d0b01dcbfef66ad74327adb40afd8
Everett Griffiths <everett.griffiths@leafgroup.com>
2018-07-10 21:26:16 -0700

    Shortening descriptions

--- elements/snippets/asset.php
+++ elements/snippets/asset.php
@@ -8,7 +8,7 @@
  * If the snippet call sets a &width but no &height, then return a scaled version of the asset scaled to the desired width.
  * If the snippet call sets a &height but no &width, then return a scaled version of the asset scaled to the desired height.
  * If both &height and &width are set, then do what the "scale" Snippet does and scale asset to the desired dimensions
- * 
+ *
  * USAGE EXAMPLES
  * [[Asset? &asset_id=`123` &tpl=`<img src="[[+url]]"/>` &height=`100` &width=`100` ]]
  * [[Asset? &asset_id=`123` &tpl=`<a href="[[+url]]"><img src="[[+thumbnail_url]]"/></a>` &height=`100` &width=`100` ]]
@@ -20,7 +20,7 @@
  *
  * Parameters
  * -----------------------------
- * @param integer &asset_id 
+ * @param integer &asset_id
  * @param integer &width in pixels
  * @param integer &height in pixels
  * @param string &tpl either a MODX chunk or a formatting string
@@ -32,67 +32,68 @@
  *
  * @package assman
  */
-$core_path = $modx->getOption('assman.core_path', null, MODX_CORE_PATH.'components/assman/');
-require_once $core_path .'vendor/autoload.php';
+$core_path = $modx->getOption('assman.core_path', null, MODX_CORE_PATH . 'components/assman/');
+require_once $core_path . 'vendor/autoload.php';
 $Snippet = new \Assman\Snippet($modx);
-$Snippet->log('Asset',$scriptProperties);
+$Snippet->log('Asset', $scriptProperties);
 
 
-$asset_id = (int) $modx->getOption('asset_id', $scriptProperties);
-$width = (int) $modx->getOption('width', $scriptProperties);
-$height = (int) $modx->getOption('height', $scriptProperties);
-$tpl = $modx->getOption('tpl', $scriptProperties, '<img src="[[+url]]" width="[[+width]]" height="[[+height]]" alt="[[+alt]]" />', true);
+$asset_id = (int)$modx->getOption('asset_id', $scriptProperties);
+$width = (int)$modx->getOption('width', $scriptProperties);
+$height = (int)$modx->getOption('height', $scriptProperties);
+$tpl = $modx->getOption('tpl', $scriptProperties,
+    '<img src="[[+url]]" width="[[+width]]" height="[[+height]]" alt="[[+alt]]" />', true);
 
 $Asset = $modx->getObject('Asset', $asset_id);
 
-if(!$Asset) {
-	$modx->log(\modX::LOG_LEVEL_DEBUG, "No results found",'','Asset',__LINE__);
-	return;
+if (!$Asset) {
+    $modx->log(\modX::LOG_LEVEL_DEBUG, "No results found", '', 'Asset', __LINE__);
+    return;
 }
 
 
 $ass_props = array(
-	'asset_id'	=> $Asset->get('asset_id'),
-	'title'	=> $Asset->get('title'),
-	'alt'	=> $Asset->get('alt'),
-	'width'	=> $Asset->get('width'),
-	'height'	=> $Asset->get('height'),
-	'thumbnail_url'	=> $Asset->get('thumbnail_url'),
-	'url'	=> $Asset->get('url')
+    'asset_id' => $Asset->get('asset_id'),
+    'title' => $Asset->get('title'),
+    'alt' => $Asset->get('alt'),
+    'width' => $Asset->get('width'),
+    'height' => $Asset->get('height'),
+    'thumbnail_url' => $Asset->get('thumbnail_url'),
+    'url' => $Asset->get('url')
 );
 
-if($Asset->is_image) {
+if ($Asset->is_image) {
 
-	if( $width == 0 && $height > 0 ) {
-		// Calculate the new dimensions
-		$nx = floor($height * ( $Asset->get('width') / $Asset->get('height') ));
-		$ny = $height;
-		$ass_props['url'] = $Asset->getResizedImage($Asset->get('path'), $asset_id,$nx,$ny);
-		$ass_props['width'] = $nx;
-		$ass_props['height'] = $ny;
-	}
+    if ($width == 0 && $height > 0) {
+        // Calculate the new dimensions
+        $nx = floor($height * ($Asset->get('width') / $Asset->get('height')));
+        $ny = $height;
+        $ass_props['url'] = $Asset->getResizedImage($Asset->get('path'), $asset_id, $nx, $ny);
+        $ass_props['width'] = $nx;
+        $ass_props['height'] = $ny;
+    }
 
-	if( $height == 0 && $width > 0 ) {
-		// Calculate the new dimensions
-		$nx = $width;
-		$ny = floor($width * ($Asset->get('height') / $Asset->get('width')));
-		$ass_props['url'] = $Asset->getResizedImage($Asset->get('path'), $asset_id,$nx,$ny);
-		$ass_props['width'] = $nx;
-		$ass_props['height'] = $ny;
-	}
+    if ($height == 0 && $width > 0) {
+        // Calculate the new dimensions
+        $nx = $width;
+        $ny = floor($width * ($Asset->get('height') / $Asset->get('width')));
+        $ass_props['url'] = $Asset->getResizedImage($Asset->get('path'), $asset_id, $nx, $ny);
+        $ass_props['width'] = $nx;
+        $ass_props['height'] = $ny;
+    }
 
-	if( $height > 0 && $width > 0 ) {
-		$ass_props['url'] = $Asset->getResizedImage($Asset->get('path'), $asset_id,$width,$height);
-		$ass_props['width'] = $width;
-		$ass_props['height'] = $height;
-	}
+    if ($height > 0 && $width > 0) {
+        $ass_props['url'] = $Asset->getResizedImage($Asset->get('path'), $asset_id, $width, $height);
+        $ass_props['width'] = $width;
+        $ass_props['height'] = $height;
+    }
 }
 
 // Create the temporary chunk
 $uniqid = uniqid();
 $chunk = $modx->newObject('modChunk', array('name' => "{tmp}-{$uniqid}"));
 $chunk->setCacheable(false);
- 
+
 $output = $chunk->process($ass_props, $tpl);
 
 return $output;
--- elements/snippets/resize.php
+++ elements/snippets/resize.php
@@ -5,38 +5,38 @@
  *
  * USAGE:
  *
- * Apply this filter to the raw asset_id to manipulate the URL inside a product page or chunks that 
+ * Apply this filter to the raw asset_id to manipulate the URL inside a product page or chunks that
  * format assets.
  *
  * <img src="[[+asset_id:resize=`500x300`]]" width="500" width="300" />
  *
  * @package assman
  */
-$modx->log(\modX::LOG_LEVEL_DEBUG, "scriptProperties:\n".print_r($scriptProperties,true),'','Snippet resize');
+$modx->log(\modX::LOG_LEVEL_DEBUG, "scriptProperties:\n" . print_r($scriptProperties, true), '', 'Snippet resize');
 
-$core_path = $modx->getOption('assman.core_path', null, MODX_CORE_PATH.'components/assman/');
-require_once $core_path .'vendor/autoload.php';
+$core_path = $modx->getOption('assman.core_path', null, MODX_CORE_PATH . 'components/assman/');
+require_once $core_path . 'vendor/autoload.php';
 
 $asset_id = $input;
 
 if (!is_numeric($asset_id)) {
-    $modx->log(\modX::LOG_LEVEL_ERROR,'Invalid input. Integer asset ID required. ' .print_r($scriptProperties,true),'','resize Output Filer');
+    $modx->log(\modX::LOG_LEVEL_ERROR, 'Invalid input. Integer asset ID required. ' . print_r($scriptProperties, true),
+        '', 'resize Output Filer');
     return;
-} 
+}
 // e.g. 500x300
-if (preg_match('/^(\d+)x(\d+)$/',$options,$m)) {
+if (preg_match('/^(\d+)x(\d+)$/', $options, $m)) {
     $w = $m[1];
     $h = $m[2];
-}
-else {
-    $modx->log(\modX::LOG_LEVEL_ERROR,'Invalid image dimensions passed: '.$options,'','resize Output Filer');
+} else {
+    $modx->log(\modX::LOG_LEVEL_ERROR, 'Invalid image dimensions passed: ' . $options, '', 'resize Output Filer');
     $w = $modx->getOption('assman.thumbnail_width');
     $h = $modx->getOption('assman.thumbnail_height');
 }
 
 if (!$Asset = $modx->getObject('Asset', array('asset_id' => $asset_id))) {
-    $modx->log(\modX::LOG_LEVEL_ERROR,'Asset not found.','','resize Output Filer');
-    return $Asset->getMissingThumbnail($w,$h);
+    $modx->log(\modX::LOG_LEVEL_ERROR, 'Asset not found.', '', 'resize Output Filer');
+    return $Asset->getMissingThumbnail($w, $h);
 }
 
 return $Asset->getThumbnailURL($w, $h);
